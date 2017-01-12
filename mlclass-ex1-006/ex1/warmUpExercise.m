58e381aaa30efad5ad1711bec8c362933f4eb9ac
ABDUL MOHSIN SIDDIQI <tarzilianams@gmail.com>
2017-01-12 20:51:20 +0530

    Add files via upload

---
+++ mlclass-ex1-006/ex1/warmUpExercise.m
@@ -0,0 +1,23 @@
+function A = warmUpExercise()
+%WARMUPEXERCISE Example function in octave
+%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = [];
+% ============= YOUR CODE HERE ==============
+% Instructions: Return the 5x5 identity matrix 
+%               In octave, we return values by defining which variables
+%               represent the return values (at the top of the file)
+%               and then set them accordingly. 
+
+%A = [1 2 3 4 5; 6 7 8 9 10; 11 12 13 14 15; 16 17 18 19 20; 21 22 23 24 25];
+A = eye(5);
+disp(A);
+
+
+
+
+
+% ===========================================
+
+
+end
